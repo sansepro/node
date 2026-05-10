56e5a3f7b53ad342186702a7da5b493de00b4c85
sansepro <sanse.pro@yandex.ru>
2026-05-11 02:00:40 +0500

    updated sysctl

--- install.sh
+++ install.sh
@@ -160,6 +160,39 @@ net.ipv4.conf.default.rp_filter = 2
 
 net.ipv6.conf.all.use_tempaddr = 0
 net.ipv6.conf.default.use_tempaddr = 0
+
+
+
+net.core.somaxconn = 65535
+net.ipv4.tcp_max_syn_backlog = 8192
+
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+
+net.ipv4.tcp_fin_timeout = 15
+
+net.ipv4.tcp_tw_reuse = 1
+
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_intvl = 30
+net.ipv4.tcp_keepalive_probes = 5
+
+net.ipv4.tcp_syncookies = 1
+
+net.core.netdev_max_backlog = 250000
+
+net.ipv4.tcp_max_orphans = 262144
+
+net.netfilter.nf_conntrack_max = 262144
+
+net.ipv4.ip_local_port_range = 1024 65535
+
+net.ipv4.tcp_mtu_probing = 1
+
+net.ipv4.tcp_slow_start_after_idle = 0
 EOF
 sysctl -p /opt/sysctl.conf
 }
